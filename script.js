f1599fb30849a29a4159d1237a8e2a9a5b2aec4c
ZIED DAMMAK <zied.dammak@enis.tn>
2023-12-10 06:02:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,262 @@
+// Get current sensor readings when the page loads  
+window.addEventListener('load', getReadings);
+
+// Create Niveau de remplissage Gauge
+var gaugeTemp = new LinearGauge({
+  renderTo: 'gauge-temperature',
+  width: 120,
+  height: 400,
+  units: "en %",
+  minValue: 0,
+  startAngle: 90,
+  ticksAngle: 180,
+  maxValue: 100,
+  colorValueBoxRect: "#049faa",
+  colorValueBoxRectEnd: "#049faa",
+  colorValueBoxBackground: "#f1fbfc",
+  valueDec: 2,
+  valueInt: 2,
+  majorTicks: [
+      "0",
+      "20",
+      "40",
+      "60",
+      "80",
+      "100"
+  ],
+  minorTicks: 4,
+  strokeTicks: true,
+  highlights: [
+      {
+          "from": 60,
+          "to": 100,
+          "color": "rgba(200, 50, 50, .75)"
+      }
+  ],
+  colorPlate: "#fff",
+  colorBarProgress: "#CC2936",
+  colorBarProgressEnd: "#049faa",
+  borderShadowWidth: 0,
+  borders: false,
+  needleType: "arrow",
+  needleWidth: 2,
+  needleCircleSize: 7,
+  needleCircleOuter: true,
+  needleCircleInner: false,
+  animationDuration: 1500,
+  animationRule: "linear",
+  barWidth: 10,
+  value:30
+}).draw();
+  
+// Create Qualite d'air Gauge
+var gaugeHum = new RadialGauge({
+  renderTo: 'gauge-humidity',
+  width: "250%",
+  height: "250%",
+  units: "Pollution (%)",
+  minValue: 0,
+  maxValue: 100,
+  colorValueBoxRect: "#049faa",
+  colorValueBoxRectEnd: "#049faa",
+  colorValueBoxBackground: "#f1fbfc",
+  valueInt: 2,
+  majorTicks: [
+      "0",
+      "20",
+      "40",
+      "60",
+      "80",
+      "100"
+
+  ],
+  minorTicks: 4,
+  strokeTicks: true,
+  highlights: [
+      {
+          "from": 80,
+          "to": 100,
+          "color": "#03C0C1"
+      }
+  ],
+  colorPlate: "#fff",
+  borderShadowWidth: 0,
+  borders: false,
+  needleType: "line",
+  colorNeedle: "#007F80",
+  colorNeedleEnd: "#007F80",
+  needleWidth: 2,
+  needleCircleSize: 3,
+  colorNeedleCircleOuter: "#007F80",
+  needleCircleOuter: true,
+  needleCircleInner: false,
+  animationDuration: 1500,
+  animationRule: "linear",
+  value:20
+}).draw();
+
+
+// Create Fire Gauge
+var gaugeFire = new RadialGauge({
+  renderTo: 'gauge-fire',
+  width: "250%",
+  height: "250%",
+  units: "Fire",
+  minValue: 0,
+  maxValue: 1,
+  colorValueBoxRect: "#049faa",
+  colorValueBoxRectEnd: "#049faa",
+  colorValueBoxBackground: "#f1fbfc",
+  valueInt: 2,
+  majorTicks: [
+      "0",
+      "1",
+
+
+  ],
+  minorTicks: 1,
+  strokeTicks: true,
+  highlights: [
+      {
+          "from": 0,
+          "to": 0.5,
+          "color": "#03C0C1"
+      }
+      ,{
+        "from": 0.5,
+          "to": 1,
+          "color": "#DB5461"
+
+      }
+  ],
+  colorPlate: "#fff",
+  borderShadowWidth: 0,
+  borders: false,
+  needleType: "line",
+  colorNeedle: "#007F80",
+  colorNeedleEnd: "#007F80",
+  needleWidth: 2,
+  needleCircleSize: 3,
+  colorNeedleCircleOuter: "#007F80",
+  needleCircleOuter: true,
+  needleCircleInner: false,
+  animationDuration: 1500,
+  animationRule: "linear"
+}).draw();
+
+// Create niveau extincteur Gauge
+var gaugeSolHum = new RadialGauge({
+  renderTo: 'gauge-sol-humidity',
+  width: "250%",
+  height: "250%",
+  units: "Niveau (%)",
+  minValue: 0,
+  maxValue: 100,
+  colorValueBoxRect: "#049faa",
+  colorValueBoxRectEnd: "#049faa",
+  colorValueBoxBackground: "#f1fbfc",
+  valueInt: 2,
+  majorTicks: [
+    "0",
+    "20",
+    "40",
+    "60",
+    "80",
+    "100"
+  ],
+  minorTicks: 2,
+  strokeTicks: true,
+  highlights: [
+      {
+          "from": 0,
+          "to": 40,
+          "color": "#DB5461"
+      }
+  ],
+  colorPlate: "#fff",
+  borderShadowWidth: 0,
+  borders: false,
+  needleType: "line",
+  colorNeedle: "#007F80",
+  colorNeedleEnd: "#007F80",
+  needleWidth: 2,
+  needleCircleSize: 3,
+  colorNeedleCircleOuter: "#007F80",
+  needleCircleOuter: true,
+  needleCircleInner: false,
+  animationDuration: 1500,
+  animationRule: "linear",
+  value:100
+}).draw();
+
+
+// Function to get current readings on the webpage when it loads for the first time
+function getReadings(){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+      console.log(myObj);
+      var fire = myObj.fire;
+
+      gaugeFire.value = fire ; 
+    }
+  }; 
+  xhr.open("GET", "/readings", true);
+  xhr.send();
+}
+
+if (!!window.EventSource) {
+  var source = new EventSource('/events');
+  
+  source.addEventListener('open', function(e) {
+    console.log("Events Connected");
+  }, false);
+
+  source.addEventListener('error', function(e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
+  
+  source.addEventListener('message', function(e) {
+    console.log("message", e.data);
+  }, false);
+  
+  source.addEventListener('new_readings', function(e) {
+    console.log("new_readings", e.data);
+    var myObj = JSON.parse(e.data);
+    console.log(myObj);
+    gaugeFire.value = myObj.fire ; 
+  }, false);
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
